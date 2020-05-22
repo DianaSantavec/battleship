a436cd703c37f9108ad958cf016b2bdf0f1460c1
SpiritasX <deja.bogovac@gmail.com>
2020-05-22 09:08:18 +0200

    Added comments

--- boardLibrary.c
+++ boardLibrary.c
@@ -1,8 +1,21 @@
 #include "battleship.h"
 
+/*
+ * Iterates through all board cells and initializes it's symbol and coordinates
+ *
+ * Parameters:
+ *     board - matrix of cells
+ *
+ * Return (void):
+ *     Nothing
+ */
+
 void initializeBoard(cell board[][COLS]) {
+	// Loop through all rows
 	for (int i = 0; i < ROWS; i++) {
+		// Loop through all columns
 		for (int j = 0; j < COLS; j++) {
+			// Initialize cell
 			board[i][j].symbol		= WATER;
 			board[i][j].position.x	= i;
 			board[i][j].position.y	= j;
@@ -10,99 +23,172 @@ void initializeBoard(cell board[][COLS]) {
     }
 }
 
-void printBoard(cell board[][COLS], bool showPegs) {
-	printf("     ");
-	printf(SYMBOL_UPPER_LEFT_EDGE);
-    for(int i = 0; i < COLS * 2 - 1; i++) {
-        if(i % 2) printf(UPPER_BORDER_T);
-        else printf(HORIZONTAL_LINE);
-    }
-    printf(SYMBOL_UPPER_RIGHT_EDGE);
-	printf("\n");
-	printf("     ");
-	printf(SINGLE_VERTICAL_LINE);
-    for(int i = 0; i < COLS * 2 - 1; i++) {
-        if(i % 2) printf(SINGLE_VERTICAL_LINE);
-        else printf("%2d ", (i + 1) / 2);
-    }
-    printf(SINGLE_VERTICAL_LINE);
-	printf("\n");
+/*
+ * Prints out a given board
+ *
+ * Parameters:
+ *     board - matrix of cells
+ *     show  - determines whether to show placed ships or no (true/false)
+ *
+ * Notes: The whole function is divided in 2 section, X axis and Y axis + board.
+ *       Each section is divided in 3 layers, top layer, middle layer and bottom layer.
+ *       Each layer has 3 parts, left edge, middle and right edge.
+ *
+ * Return (void):
+ *     Nothing
+ */
 
-	printf("     ");
-	printf(SYMBOL_LOWER_LEFT_EDGE);
+void printBoard(cell board[][COLS], bool show) {
+	// SECTION 1
+		// Top layer
+			// Left edge
+				printf("     ");
+				printf(SYMBOL_UPPER_LEFT_EDGE);
+			// Middle
+			    for(int i = 0; i < COLS * 2 - 1; i++) {
+			        if(i % 2) printf(UPPER_BORDER_T);
+			        else printf(HORIZONTAL_LINE);
+			    }
+			// Right edge
+			    printf(SYMBOL_UPPER_RIGHT_EDGE);
+				printf("\n");
+		// Middle layer
+			// Left edge
+				printf("     ");
+				printf(SINGLE_VERTICAL_LINE);
+			// Middle
+			    for(int i = 0; i < COLS * 2 - 1; i++) {
+			        if(i % 2) printf(SINGLE_VERTICAL_LINE);
+			        else printf("%2d ", (i + 1) / 2);
+			    }
+			// Right edge
+			    printf(SINGLE_VERTICAL_LINE);
+				printf("\n");
+		// Bottom layer
+			// Left edge
+				printf("     ");
+				printf(SYMBOL_LOWER_LEFT_EDGE);
+			// Middle
+			    for(int i = 0; i < COLS * 2 - 1; i++) {
+			        if(i % 2) printf(LOWER_BORDER_T);
+			        else printf(HORIZONTAL_LINE);
+			    }
+			// Right edge
+			    printf(SYMBOL_LOWER_RIGHT_EDGE);
+				printf("\n");
+	// SECTION 2
+		// Top layer
+			// Y axis
+				// Left edge
+					printf(SYMBOL_UPPER_LEFT_EDGE);
+				// Middle
+					printf(HORIZONTAL_LINE);
+				// Right edge
+					printf(SYMBOL_UPPER_RIGHT_EDGE);
 
-    for(int i = 0; i < COLS * 2 - 1; i++) {
-        if(i % 2) printf(LOWER_BORDER_T);
-        else printf(HORIZONTAL_LINE);
-    }
-    printf(SYMBOL_LOWER_RIGHT_EDGE);
-	printf("\n");
-
-	printf(SYMBOL_UPPER_LEFT_EDGE);
-	printf(HORIZONTAL_LINE);
-	printf(SYMBOL_UPPER_RIGHT_EDGE);
-
-    printf(SYMBOL_UPPER_LEFT_EDGE);
-    for(int i = 0; i < COLS * 2 - 1; i++) {
-        if(i % 2) printf(SYMBOL_T_SECOND_TYPE);
-        else printf(HORIZONTAL_LINE);
-    }
-    printf(SYMBOL_UPPER_RIGHT_EDGE);
-	printf("\n");
-
-    for(int i = 0; i < ROWS * 2 - 1; i++) {
-        if(i % 2) {
-			printf(SYMBOL_T_ROTATED_RIGHT);
-			printf(HORIZONTAL_LINE);
-			printf(SYMBOL_T_ROTATED_LEFT);
-
-            printf(SYMBOL_T_ROTATED_RIGHT_SECOND);
-            for(int j = 0; j < COLS * 2 - 1; j++)
-                if(j % 2) printf(SYMBOL_CROSS);
-				else printf(HORIZONTAL_LINE_SINGLE);
-            printf(SYMBOL_T_ROTATED_LEFT_SECOND);
-			printf("\n");
-        } else {
-			printf(SYMBOL_VERTICAL_LINE);
-			printf(" %c ", (i+1)/2 + 'A');
-			printf(SYMBOL_VERTICAL_LINE);
-
-            printf(SYMBOL_VERTICAL_LINE);
-            for(int j = 0; j < COLS * 2 - 1; j++) {
-                if(j % 2) printf(SYMBOL_VERTICAL_LINE_SINGLE);
-				else
-					switch(board[(i + 1) / 2][(j + 1) / 2].symbol) {
-						case HIT:
-						case MISS:
-						case WATER:
-							printf(" %c ", board[(i + 1) / 2][(j + 1) / 2].symbol);
-							break;
-						default:
-							if(showPegs)
-								printf(" %c ", board[(i + 1) / 2][(j + 1) / 2].symbol);
-							else
-								printf(" %c ", WATER);
-							break;
-					}
-			}
-            printf(SYMBOL_VERTICAL_LINE);
-			printf("\n");
-        }
-    }
-	printf(SYMBOL_LOWER_LEFT_EDGE);
-	printf(HORIZONTAL_LINE);
-	printf(SYMBOL_LOWER_RIGHT_EDGE);
-
-    printf(SYMBOL_LOWER_LEFT_EDGE);
-    for(int i = 0; i < COLS * 2 - 1; i++) {
-        if(i % 2) printf(SYMBOL_T_ROTATED_SECOND);
-        else printf(HORIZONTAL_LINE);
-    }
-    printf(SYMBOL_LOWER_RIGHT_EDGE);
-	printf("\n");
+			// Board
+				// Left edge
+			    	printf(SYMBOL_UPPER_LEFT_EDGE);
+				// Middle
+				    for(int i = 0; i < COLS * 2 - 1; i++) {
+				        if(i % 2) printf(SYMBOL_T_SECOND_TYPE);
+				        else printf(HORIZONTAL_LINE);
+				    }
+				// Right edge
+				    printf(SYMBOL_UPPER_RIGHT_EDGE);
+					printf("\n");
+		// Middle layer
+		    for(int i = 0; i < ROWS * 2 - 1; i++) {
+		        if(i % 2) {
+					// Y axis
+						// Left edge
+							printf(SYMBOL_T_ROTATED_RIGHT);
+						// Middle
+							printf(HORIZONTAL_LINE);
+						// Right edge
+							printf(SYMBOL_T_ROTATED_LEFT);
+					// Board
+						// Left edge
+			            	printf(SYMBOL_T_ROTATED_RIGHT_SECOND);
+						// Middle
+				            for(int j = 0; j < COLS * 2 - 1; j++) {
+				                if(j % 2) printf(SYMBOL_CROSS);
+								else printf(HORIZONTAL_LINE_SINGLE);
+							}
+						// Right edge
+				            printf(SYMBOL_T_ROTATED_LEFT_SECOND);
+							printf("\n");
+		        } else {
+					// Y axis
+						// Left edge
+							printf(SYMBOL_VERTICAL_LINE);
+						// Middle
+							printf(" %c ", (i+1)/2 + 'A');
+						// Right edge
+							printf(SYMBOL_VERTICAL_LINE);
+					// Board
+						// Left edge
+			            	printf(SYMBOL_VERTICAL_LINE);
+						// Middle
+				            for(int j = 0; j < COLS * 2 - 1; j++) {
+				                if(j % 2) printf(SYMBOL_VERTICAL_LINE_SINGLE);
+								else
+									switch(board[(i + 1) / 2][(j + 1) / 2].symbol) {
+										case HIT:
+										case MISS:
+										case WATER:
+											printf(" %c ", board[(i + 1) / 2][(j + 1) / 2].symbol);
+											break;
+										default:
+											if(show)
+												printf(" %c ", board[(i + 1) / 2][(j + 1) / 2].symbol);
+											else
+												printf(" %c ", WATER);
+											break;
+									}
+							}
+						// Right edge
+				            printf(SYMBOL_VERTICAL_LINE);
+							printf("\n");
+		        }
+		    }
+		// Bottom layer
+			// Y axis
+				// Left edge
+					printf(SYMBOL_LOWER_LEFT_EDGE);
+				// Middle
+					printf(HORIZONTAL_LINE);
+				// Right edge
+					printf(SYMBOL_LOWER_RIGHT_EDGE);
+			// Board
+				// Left edge
+			    	printf(SYMBOL_LOWER_LEFT_EDGE);
+				// Middle
+				    for(int i = 0; i < COLS * 2 - 1; i++) {
+				        if(i % 2) printf(SYMBOL_T_ROTATED_SECOND);
+				        else printf(HORIZONTAL_LINE);
+				    }
+				// Right edge
+				    printf(SYMBOL_LOWER_RIGHT_EDGE);
+					printf("\n");
 }
 
-void updateBoard (cell board[][COLS], coordinates target) {
+/*
+ * Updates board's symbols with new target
+ *
+ * Parameters:
+ *     board  - matrix of cells
+ *     target - coordinates of a cell to update
+ *
+ * Notes: If targeted water, sets symbol to MISS.
+ *       If targeted ship, sets symbol to HIT.
+ *       Everything else is ignored.
+ *
+ * Return (void):
+ *     Nothing
+ */
+
+void updateCell (cell board[][COLS], coordinates target) {
 	switch (board[target.x][target.y].symbol) {
 		case WATER:
 			board[target.x][target.y].symbol = MISS;
--- coordinatesFunctions.c
+++ coordinatesFunctions.c
@@ -1,5 +1,15 @@
 #include "battleship.h"
 
+/*
+ * Handles user input of coordinates.
+ * Each input is supposed to be in form of 2 characters.
+ * First character represents Y axis and is a letter from A to J (doesn't matter if it's capital or not).
+ * Second character represents X axis and is a number from 0 to 9.
+ *
+ * Return (coordinates):
+ *     Newly inputed coordinates
+ */
+
 coordinates inputCoordinate() {
     coordinates input;
 	char temp[3];
@@ -10,20 +20,35 @@ coordinates inputCoordinate() {
     	printf ("> Enter field(ex. A8):\n");
     	printf ("> ");
     	scanf ("%2s", temp);
+        // Format first character
         temp[0] = toupper(temp[0]) - 'A';
+        // If wrong input, return to the beginning.
         if(temp[0] < 0 || temp[0] > 9) continue;
+        // Format second character
         temp[1] = temp[1] - '0';
+        // If wrong input, return to the beginning.
 		if(temp[1] < 0 || temp[1] > 9) continue;
+        // Save new coordinates
 		input.x = temp[0];
 		input.y = temp[1];
+        // End
         break;
     } while(true);
 	return input;
 }
 
-int checkShot (cell gameBoard[][COLS], coordinates target) {
+/*
+ * Checks what's hit on the board
+ *
+ * Return (int):
+ *     0  - hit WATER
+ *     1  - hit any ship
+ *     -1 - invalid target
+ */
+
+int checkShot (cell board[][COLS], coordinates target) {
 	int hit = -2;
-	switch (gameBoard[target.x][target.y].symbol) {
+	switch (board[target.x][target.y].symbol) {
 		case WATER:
 			hit = 0;
 			break;
